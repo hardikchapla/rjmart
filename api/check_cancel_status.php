20796c168b4873adecd60687f03f364d3a234d8f
Hardik Chapla <chaplahardik009@gmail.com>
2022-06-10 16:49:41 +0530

    update 10-06-22

---
+++ api/check_cancel_status.php
@@ -0,0 +1,49 @@
+<?php
+	include "../connection/connection.php";
+	include "../helper/constant.php";
+	$status = 0;
+	$message = "";
+	$data = array();
+	$response = array();
+	if(!empty($_REQUEST)){
+ 		if(empty($_REQUEST['order_id'])){
+ 			$status = 2;
+			$message = "Please enter order id";
+			$data = array();
+			$response['status'] = $status;
+			$response['message'] = $message;
+			$response['data'] = $data;
+			echo json_encode($response);
+			die;
+ 		}
+ 		$order_id = $_REQUEST['order_id'];
+ 		$getOrderId = $db->query("SELECT * FROM product_order WHERE id = '$order_id'");
+        $feOrder = $getOrderId->fetch();
+        $orderTime =  $feOrder['order_date'];
+        date_default_timezone_set('Asia/Kolkata');
+        $currentDateTime = date("Y-m-d H:i:s");
+
+        $t1 = strtotime($orderTime);
+        $t2 = strtotime($currentDateTime);
+        $interval  = abs($t2 - $t1);
+        $minutes   = round($interval / 60);
+        
+        $getAdminCancelTime = $db->query("SELECT * FROM `admin` WHERE id = '1'");
+        $feCancelTime = $getAdminCancelTime->fetch();
+        $cancelMinutes = $feCancelTime['cancel_time'];
+
+        if($minutes <= $cancelMinutes ){
+            $status = 1;
+            $message = "You can cancel your order";
+        }else{
+            $status = 0;
+            $message = "You can't cancel your order because cancellation time limit was " .$cancelMinutes. " minutes";
+        }
+        
+	}
+    $response['status'] = $status;
+	$response['message'] = $message;
+	$response['data'] = $data;
+	echo json_encode($response);
+	die;
+?>
