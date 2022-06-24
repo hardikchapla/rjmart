3022cbe3bb8f6a6000f418b45363a900e4be022b
Hardik Chapla <chaplahardik009@gmail.com>
2022-06-24 17:10:55 +0530

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -107,8 +107,21 @@ var fridayCount = "<?php echo $fridayCount; ?>";
     <link rel="icon" type="image/x-icon" href="<?= FAVICON ?>" />
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard/dash_1.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <link rel="stylesheet" type="text/css" href="plugins/dropify/dropify.min.css">
+    <link href="assets/css/users/account-setting.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href="assets/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
     <style>
-
+    .admin-name-header {
+        margin-top: 3px !important;
+    }
     </style>
 </head>
 
