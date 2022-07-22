9737b72f348773cf8f457e22b10e73434c0a27b1
Sanghani Chirag <csanghani58@gmail.com>
2022-07-22 09:39:28 +0530

    Updated 2022-07-22
    Changes credential on live

--- connection/connection.php
+++ connection/connection.php
@@ -1,5 +1,6 @@
 <?php
-
+	    print_r($_SERVER['HTTP_HOST']);
+		die;
 	/*
 	 * To change this license header, choose License Headers in Project Properties.
 	 * To change this template file, choose Tools | Templates
